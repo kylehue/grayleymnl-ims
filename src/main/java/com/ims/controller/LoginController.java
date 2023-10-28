e24aad8ede20e15a113b184cdf23386299e5f213
kylehue <apeuda69@gmail.com>
2023-10-28 21:12:33 +0800

    change username to email

--- src/main/java/com/ims/controller/LoginController.java
+++ src/main/java/com/ims/controller/LoginController.java
@@ -23,24 +23,36 @@ public class LoginController {
     private ImageView vectorImage;
     
     @FXML
-    private MFXButton loginButton;
-    
-    @FXML
     private Canvas networkCanvas;
     
     @FXML
-    public void initialize()
-        throws URISyntaxException, ParserConfigurationException, IOException, SAXException {
+    private MFXButton loginButton;
+    
+    @FXML
+    public MFXButton registerButton;
+    
+    @FXML
+    public void initialize() {
+        this.initializeNetworkAnimation();
         Utils.fitImageViewToParent(vectorImage);
-//        Utils.addIconToButton(loginButton, "/icons/login.svg");
         
-        // TODO: Make a separate function in the class for this
         loginButton.setOnMouseClicked((MouseEvent event) -> {
             SceneManager.setScene("base");
         });
         
+        registerButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
+            SceneManager.setScene("register");
+        });
+    }
+    
+    private void initializeNetworkAnimation() {
         Network networkAnimation = new Network(networkCanvas);
-        
-        
+        SceneManager.onChangeScene((newScene, oldScene) -> {
+            if (newScene != "login") {
+                networkAnimation.stop();
+            } else {
+                networkAnimation.start();
+            }
+        });
     }
 }
